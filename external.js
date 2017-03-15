aa32bd756a8d97db6815831b31ded121fdb9b6ce
Alexandre Grière <alexandre.griere@mobistep.com>
2017-03-15 10:01:28 +0100

    Little code refactoring, Adding green background for finished or accepted tack

--- external.js
+++ external.js
@@ -15,55 +15,56 @@ function callAPI(element, callback){
 
 	if (element)
 	{
-	// new cross origin XML request
-	chrome.storage.sync.get(null, function(items) {
-		console.log(items);
-		if (!items.projectId)
-			callback(false, element);
+		// new cross origin XML request
+		chrome.storage.sync.get(null, function(items) {
+			console.log(items);
+			if (!items.projectId)
+				callback(false, element);
 
-		if (!items.apiToken)
-			callback(false, element);
+			if (!items.apiToken)
+				callback(false, element);
 
-		var xmlhttp=new XMLHttpRequest();
-		xmlhttp.onreadystatechange=function()
-		{
-			if (xmlhttp.readyState==4)
-			{
-				if(xmlhttp.status==200)
-				{
-					fetchResult = JSON.parse(xmlhttp.responseText);
-					callback(fetchResult, element);
-				} else {
-					callback(false, element);
+			var xmlhttp=new XMLHttpRequest();
+			xmlhttp.onreadystatechange=function(){
+				if (xmlhttp.readyState==4){
+					if(xmlhttp.status==200){
+						fetchResult = JSON.parse(xmlhttp.responseText);
+						callback(fetchResult, element);
+					} else {
+						callback(false, element);
+					}
 				}
 			}
-		}
 
-	var apiId = items.apiToken;
-	var projectId = items.projectId;
-	var search = element.getAttribute('data-rm-id');
+			var apiId = items.apiToken;
+			var projectId = items.projectId;
+			var search = element.getAttribute('data-rm-id');
 
-	
-	xmlhttp.open("GET","https://www.pivotaltracker.com/services/v5/projects/" + projectId + "/search?query=" + search + "+AND+includedone%3Atrue", true);
-	xmlhttp.setRequestHeader("X-TrackerToken", apiId);
-	xmlhttp.send();
+			if (apiId && projectId) {
+				xmlhttp.open("GET","https://www.pivotaltracker.com/services/v5/projects/" + projectId + "/search?query=" + search + "+AND+includedone%3Atrue", true);
+				xmlhttp.setRequestHeader("X-TrackerToken", apiId);
+				xmlhttp.send();
+			}
+			else {
+				alert("Can't connect to Pivotal Tracker. Missing ProjetId or Token.");
+			}
 
+		});
 
-});
-
-	
-
-} 
-else {
-
-	callback(false, element);
-}
+	} 
+	else {
+		callback(false, element);
+	}
 }
 
 function parseAPI(response, element)
 {
 	if (response) {
 		if (response.stories.total_hits > 0) {
+			if (response.stories.stories[0].current_state == 'finished' || response.stories.stories[0].current_state == 'accepted') {
+				element.style.background = "Green";
+				element.style.color = "White";
+			}
 			element.innerHTML = "✓";
 		}
 		else{
